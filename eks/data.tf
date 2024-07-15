55516b64b5aaa9a983874f2a165f93a38be330e0
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-15 18:44:37 +0530

    updated

--- eks/data.tf
+++ eks/data.tf
@@ -1,9 +1,18 @@
  
+# data "terraform_remote_state" "vpc" {
+#   backend = "s3"
+#   config = {
+#     bucket  = "terraform-devops29master"
+#     key     = "${var.ENV}/vpc/terraform.tfstate"
+#     region  = "us-east-1"
+#   }
+# }
+
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket  = "terraform-devops29master"
-    key     = "${var.ENV}/vpc/terraform.tfstate"
-    region  = "us-east-1"
+    bucket = "terraform-devops29master"
+    key    = "dev/terrafile/vpc/terraform.tfstate"
+    region = "us-east-1"
   }
 }
