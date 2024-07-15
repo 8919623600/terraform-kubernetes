b9be496fd40b5b8a85f981364ded5080372c4655
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-15 18:42:35 +0530

    updated

--- eks/data.tf
+++ eks/data.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket  = "b57-tf-state-bucket"
+    bucket  = "terraform-devops29master"
     key     = "${var.ENV}/vpc/terraform.tfstate"
     region  = "us-east-1"
   }
